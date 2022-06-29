3dd4c62198523a342f0abeefd96a513562916555
Vurnex <hamilton.laquon@gmail.com>
2022-06-29 18:34:53 -0500

    Implement playRound function
    Implemented a round function that plays a single match.
    
    The function compares player and computer selections and
    determines a winner.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,48 @@
 function computerPlay() {
 
-    const options = ["Rock", "Paper", "Scissors"];
+    const options = ["rock", "paper", "scissors"];
 
+    //Math.random chooses a random number. Multiply by the array length to get length of the array. Math.floor chooses a random index.
     let chosenOption = options[Math.floor(Math.random() * options.length)];
 
     console.log(chosenOption);
+
+    return chosenOption;
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+
+    let inputCorrection = playerSelection.toLowerCase();
+    
+    //Both selections are the same
+    if (
+        (inputCorrection == "rock" && computerSelection == "rock") ||
+        (inputCorrection == "paper" && computerSelection == "paper") ||
+        (inputCorrection == "scissors" && computerSelection == "scissors")  
+    ) {
+
+        console.log("This match was a tie.");
+    }
+    else if (   //Player wins  
+        (inputCorrection == "rock" && computerSelection == "scissors") ||
+        (inputCorrection == "paper" && computerSelection == "rock") ||
+        (inputCorrection == "scissors" && computerSelection == "paper")
+    ) {
+
+        console.log("You win! " + inputCorrection + " beats " + computerSelection);
+    }
+    else if (   //Computer wins
+        (computerSelection == "rock" && inputCorrection == "scissors") ||
+        (computerSelection == "paper" && inputCorrection == "rock") ||
+        (computerSelection == "scissors" && inputCorrection == "paper")
+    ) {
+
+        console.log("You lose! " + computerSelection + " beats " + inputCorrection);
+    }
+
+}
+
+let playerSelection = prompt("Enter rock, paper, or scissors");
+const computerSelection = computerPlay();
+
+playRound(playerSelection, computerSelection);
